90e69fed1a3465f01f0d0334f4cacdb4be2d1d7b
M74JJI <52024672+M74JJI@users.noreply.github.com>
2023-03-25 13:53:54 +0000

    backend full setup with user crud (#1)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,18 +5,25 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import helmet from '@fastify/helmet';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  // TODO: contentSecurityPolicy should turn on in production
+  await app.register(helmet, { contentSecurityPolicy: false });
+  app.enableCors();
   const port = process.env.PORT || 3333;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/graphiql`);
 }
 
 bootstrap();
---
+++ libs/api/feature-user/src/lib/api-feature-user.spec.ts
@@ -0,0 +1,7 @@
+import { apiFeatureUser } from './api-feature-user';
+
+describe('apiFeatureUser', () => {
+  it('should work', () => {
+    expect(apiFeatureUser()).toEqual('api-feature-user');
+  });
+});
